820f683da3530aa8415e13d6d8e5e59fb930df85
Evren Caglayan <caglayanevren@gmail.com>
2020-09-05 20:03:30 +0300

    add fixed-top class to purgecss whitelist

--- purgecss.config.js
+++ purgecss.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     content: ["**/*.hbs"],
-    whitelist: ["body", "right", "active", "show", "is-active", "tns-slider", "tns-outer", "tns-inner"],
+    whitelist: ["body", "right", "active", "show", "is-active", "tns-slider", "tns-outer", "tns-inner", "fixed-top"],
     whitelistPatterns: [/^tns/, /^nav/],
     whitelistPatternsChildren: [/^tns/, /^nav/]
 }
