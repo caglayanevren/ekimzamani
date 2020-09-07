dba4c6839a5266f4329641b81ad8a71dcba1b40c
Evren Caglayan <caglayanevren@gmail.com>
2020-09-07 11:36:24 +0300

    add fa class to purgecss whitelist

--- purgecss.config.js
+++ purgecss.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     content: ["**/*.hbs"],
-    whitelist: ["body", "right", "active", "show", "is-active", "tns-slider", "tns-outer", "tns-inner", "fixed-top"],
+    whitelist: ["body", "right", "active", "show", "is-active", "tns-slider", "tns-outer", "tns-inner", "fixed-top", "fas", "fa-2x", "fa-arrow-left", "fa-arrow-right", "color-kahve"],
     whitelistPatterns: [/^tns/, /^nav/],
     whitelistPatternsChildren: [/^tns/, /^nav/]
 }
