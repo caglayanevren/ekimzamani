ef92b0a08277138d05fd8b152e7330e118f22eec
Evren Caglayan <caglayanevren@gmail.com>
2020-09-10 13:09:18 +0300

    navbar scrolled feature fixed

--- src/js/main.js
+++ src/js/main.js
@@ -5,20 +5,23 @@ window.$ = $;
 import "./modernizr";
 import "bootstrap";
 import "popper.js";
+import Handlebars from "handlebars";
 
 ///////////////// fixed menu on scroll for desktop
-    $(window).scroll(function(){  
-        if ($(this).scrollTop() > 74) {
-            $('#nav.navbar').addClass("fixed-top");
-            var navbarHeight = $('#nav.navbar').outerHeight();
-            // add padding top to show content behind navbar
-            $('body').css('padding-top', navbarHeight + 'px');
-        }else{
-            $('#nav.navbar').removeClass("fixed-top");
-            // remove padding top from body
-            $('body').css('padding-top', '0');
-        }   
-    });
+$(window).scroll(function () {
+    if ($(this).scrollTop() > 74) {
+        $('#nav.navbar').addClass("fixed-top");
+        var navbarHeight = $('#nav.navbar').outerHeight();
+        // add padding top to show content behind navbar
+        $('body').css('padding-top', navbarHeight + 'px');
+        $('body').addClass('scrolled');
+    } else {
+        $('#nav.navbar').removeClass("fixed-top");
+        // remove padding top from body
+        $('body').css('padding-top', '0');
+        $('body').removeClass('scrolled');
+    }
+});
 // end if
 
 $(window).on("load", function () {
@@ -30,18 +33,22 @@ $(window).on("load", function () {
     });
 });
 
-$(window).on('load', function() {
-	"use strict";
-	var viewportWidth = window.innerWidth;
-	if(viewportWidth > 1199) {
-		$('.dropdown').mouseover(function(){
-			$(this).addClass('show');
-			$(this).children('.dropdown-menu').addClass('show');
-			$(this).children('a').attr('aria-expanded','true');
-		}).mouseout(function(){
-			$(this).removeClass('show');
-			$(this).children('.dropdown-menu').removeClass('show');
-			$(this).children('a').attr('aria-expanded','false');
-		});
-	}
+$(window).on('load', function () {
+    "use strict";
+    var viewportWidth = window.innerWidth;
+    if (viewportWidth > 1199) {
+        $('.dropdown').mouseover(function () {
+            $(this).addClass('show');
+            $(this).children('.dropdown-menu').addClass('show');
+            $(this).children('a').attr('aria-expanded', 'true');
+        }).mouseout(function () {
+            $(this).removeClass('show');
+            $(this).children('.dropdown-menu').removeClass('show');
+            $(this).children('a').attr('aria-expanded', 'false');
+        });
+    }
+});
+
+Handlebars.registerHelper('scrolled', function (isScrolled) {
+    return isScrolled = $('body').hasClass('scrolled');
 });
