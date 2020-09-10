612437f3a371f255ad74ac752a83a10efd688b00
Evren Caglayan <caglayanevren@gmail.com>
2020-09-10 13:19:38 +0300

    add scrolled class to purgecss whitelist

--- purgecss.config.js
+++ purgecss.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     content: ["**/*.hbs"],
-    whitelist: ["body", "right", "active", "show", "is-active", "tns-slider", "tns-outer", "tns-inner", "fixed-top", "fas", "fa-2x", "fa-arrow-left", "fa-arrow-right", "color-kahve"],
+    whitelist: ["body", "right", "active", "show", "is-active", "tns-slider", "tns-outer", "tns-inner", "fixed-top", "fas", "fa-2x", "fa-arrow-left", "fa-arrow-right", "color-kahve", "scrolled", "unscrolled-image", "scrolled-image"],
     whitelistPatterns: [/^tns/, /^nav/],
     whitelistPatternsChildren: [/^tns/, /^nav/]
 }
