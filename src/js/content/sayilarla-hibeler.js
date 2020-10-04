110720f28cd84ce510d4264414929e419ea7992b
Evren Caglayan <caglayanevren@gmail.com>
2020-10-04 14:47:28 +0300

    fix last design issues

--- src/js/content/sayilarla-hibeler.js
+++ src/js/content/sayilarla-hibeler.js
@@ -1,4 +1,49 @@
 import "../main";
+import {
+    tns
+} from "tiny-slider/src/tiny-slider";
+
+var sayilarlaHibelerSlider = tns({
+    container: '#sayilarla-hibeler-slider',
+    items: 3,
+    autoplay: false,
+    autoplayButtonOutput: false,
+    mouseDrag: true,
+    nav: false,
+    controlsText: ["<i class='fas fa-2x fa-arrow-left color-kahve'></i>", "<i class='fas fa-2x fa-arrow-right color-kahve'></i>"],
+    responsive: {
+        425: {
+            gutter: 10,
+            controls: false
+        },
+        576: {
+            gutter: 10,
+            controls: false
+        },
+        768: {
+            gutter: 15,
+            controls: false
+        },
+        992: {
+            gutter: 20,
+            controls: true
+        },
+        1200: {
+            gutter: 30,
+            controls: true
+        }
+    }
+
+});
+
+sayilarlaHibelerSlider.getInfo().slideItems[sayilarlaHibelerSlider.getInfo().index + 1].classList.add('active--slide');
+sayilarlaHibelerSlider.events.on('indexChanged', () => {
+    const info = sayilarlaHibelerSlider.getInfo();
+    const indexPrev = info.indexCached + 1;
+    const indexCurr = info.index + 1;
+    info.slideItems[indexPrev].classList.remove('active--slide');
+    info.slideItems[indexCurr].classList.add('active--slide');
+});
 
 var svgicons = document.querySelectorAll("g.svgicon");
 svgicons.forEach(function (svgicon) {
