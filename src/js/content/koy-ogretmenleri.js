0fe7dd55a11cffbcbcfe1695de47a3e511e8c44b
Evren Caglayan <caglayanevren@gmail.com>
2020-10-10 22:30:32 +0300

    add more & more content

---
+++ src/js/content/koy-ogretmenleri.js
@@ -0,0 +1,25 @@
+import "../main";
+import {
+    tns
+} from "tiny-slider/src/tiny-slider";
+
+var koyOgretmenleriSlider = tns({
+    container: '#koyOgretmenleriSlider',
+    items: 1,
+    autoplay: true,
+    autoplayButtonOutput: false,
+    mouseDrag: true,
+    nav: true,
+    navPosition: "bottom",
+    controlsText: ["<i class='fas fa-2x fa-arrow-left color-genc'></i>", "<i class='fas fa-2x fa-arrow-right color-genc'></i>"],
+    responsive: {
+        0: {
+            controls: false,
+            arrowKeys: true
+        },
+        576: {
+            controls: true,
+            arrowKeys: true
+        }
+    }
+});
