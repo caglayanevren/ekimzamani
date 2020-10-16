ba82b29a1c381a9fa5e530b7bd56ce5776e54d72
Evren Caglayan <caglayanevren@gmail.com>
2020-10-16 15:44:27 +0300

    fix slider nav

--- src/js/infografikler.js
+++ src/js/infografikler.js
@@ -7,32 +7,37 @@ export default function infografikler() {
         autoplay: true,
         autoplayButtonOutput: false,
         mouseDrag: true,
-        nav: false,
         controlsText: ["<i class='fas fa-2x fa-arrow-left color-kahve'></i>", "<i class='fas fa-2x fa-arrow-right color-kahve'></i>"],
         responsive: {
             0: {
                 gutter: 5,
                 controls: false,
+                nav: true,
             },
             425: {
                 gutter: 10,
                 controls: false,
+                nav: true,
             },
             576: {
                 gutter: 10,
                 controls: false,
+                nav: true,
             },
             768: {
                 gutter: 15,
                 controls: false,
+                nav: true,
             },
             992: {
                 gutter: 20,
                 controls: true,
+                nav: false,
             },
             1200: {
                 gutter: 30,
                 controls: true,
+                nav: false,
             },
         },
     });
